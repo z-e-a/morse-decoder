8ee5706e162823015b5c297c41a525e097331640
z-e-a <e.a.zubkov@yandex.ru>
2021-08-23 11:53:02 +0300

    feat: solve task 'Morse code decoder'

--- src/index.js
+++ src/index.js
@@ -38,9 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    while(expr.length >= 10)
+    {
+        let symb = expr.substr(0, 10);
+        if (symb === '**********') {
+            result += ' ';
+        } else {
+            result += MORSE_TABLE[symb.replace(/10/g, '.').replace(/11/g, '-').replace(/00/g, '')]
+        }
+        expr = expr.substring(10);
+    }
+    return result;
 }
 
 module.exports = {
     decode
 }
+
+// debugger
+// const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+// console.log(decode(expr));
+//should return "hello world";
